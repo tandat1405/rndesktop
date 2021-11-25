fe2cb52902bda858fac3bafd1161079ace62f6e9
DESKTOP-Q6GMRE6\Wavy <tandat1405@gmail.com>
2021-11-25 13:33:13 +0700

    first commit

---
+++ windows/rndesktop/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::rndesktop::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::rndesktop::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
